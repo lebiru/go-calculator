a088130a155893ddf52d4607e7a6d8a3829e23ec
Lebiru <bilal.akabi@gmail.com>
2021-07-02 20:10:40 -0400

    remove dead code

--- server.go
+++ server.go
@@ -7,21 +7,6 @@ import (
 	"strconv"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintf(w, "Hello!\n")
-}
-
 func addHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err := r.ParseForm(); err != nil {
@@ -62,27 +47,10 @@ func subtractHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err:%v", err)
-		return
-	}
-	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
-
-}
-
 func main() {
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/add", addHandler)
 	http.HandleFunc("/subtract", subtractHandler)
 
